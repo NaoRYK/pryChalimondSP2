2174ac05242b7b3aebdb382e7fb0c43a1852fe4b
NaoRYK <dragonarcher2005@gmail.com>
2025-09-24 12:55:48 -0300

    Adding basic load

--- pryChalimondSP2/frmReserva.cs
+++ pryChalimondSP2/frmReserva.cs
@@ -25,10 +25,34 @@ namespace pryChalimondSP2
 
 
         private void frmReserva_Load(object sender, EventArgs e)
-        {
+        
+         {
+            // cargar los items en el primer control combobox
+            lstShackType.Items.Clear();
+            lstShackType.Items.Add("Tipo A");
+            lstShackType.Items.Add("Tipo B");
+            // esta acción provoca el disparo del evento "SelectedIndexChanged"
+            lstShackType.SelectedIndex = 0;
+                // se inicializa la cantidad de días en 1
+                numDays.Text = "1";
+                // inicialzar los demás controles de la interfaz
+                chkKitchen.Checked = false;
+                chkFridge.Checked = false;
+                chkTv.Checked = false;
+                // en los radiobuttons se asigna sólo el que debe quedar en true
+                radioCash.Checked = true;
+                txtName.Text = "";
+                txtPhone.Text = "";
+                // cargar los items del combo de tarjetas
+                lstCardList.Items.Clear();
+            lstCardList.Items.Add("Card Red");
+            lstCardList.Items.Add("Card Green");
+            lstCardList.Items.Add("Card Blue");
+                // deshabiliar el botón "Aceptar"
+                btnSumbit.Enabled = false;
+         }
 
-
-        }
+        
 
 
     }
